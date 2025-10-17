64acd84c11f2c52a47a7c6fa6187c7ee104cd7bb
Floyd Price <floyd@hampton.io>
2025-10-17 02:56:46 +0100

    Use official Vercel preset for React Router v7
    - Install @vercel/react-router package
    - Create react-router.config.ts with vercelPreset
    - Simplify vercel.json to use react-router framework
    - Remove custom api/index.js (not needed with preset)
    
    This is the official way to deploy React Router v7 to Vercel.
    Ref: https://vercel.com/changelog/support-for-react-router-v7

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel serverless function entry point
-import pkg from "@react-router/node";
-const { createRequestHandler } = pkg;
-import * as build from "../build/server/index.js";
-
-export default createRequestHandler({ build });
-
